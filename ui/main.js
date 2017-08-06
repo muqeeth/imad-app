cfc48bd5972d8f30ec159efd7a470f4ca902aea3
muqeeth <muqeeth101@gmail.com>
2017-08-06 12:11:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,18 +19,36 @@ button.onclick = function(){
   request.send(null);
   
 };
-var nameinput = document.getElementById("name");
+
 var submit = document.getElementById("submit_btn");
 list ="";
-var names = ["name1","name2","name3","name4"];
+
 submit.onclick=function(){
-for(var i= 0;i<names.length;i++){
-    list += '<li>'+names[i]+'</li>';
     
-}
-var ul =  document.getElementById("namelist");
-ul.innerHTML = list;
+    var request = new XMLHttpRequest();
+  request.onreadystatechange = function(){
+    if(request.readyState==XMLHttpRequest.DONE){
+        if(request.status==200){
+            var nameinput = document.getElementById("name");
+            var name = nameinput.value;
+            var names = request.responseText;
+            names = JSON.parse(names);
+            
+            var ls = [];
+            for(var i=0;i<names.length;i++){
+                ls+='<li>'+names[i]+'</li>';
+            }
+            var ul = document.getElementById("namelist");
+            ul.innerHTML = ls;
+        }
+    }
 };
+
+request.open('GET',"http://ee16b026.imad.hasura-app.io/submit-name?name="+name,true);
+request.send(null);
+
+};
+
 
 
 
