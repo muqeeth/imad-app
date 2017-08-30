7cd54a436368ab0ca37d7ccd70062c5e538d8ac7
muqeeth <muqeeth101@gmail.com>
2017-08-30 12:26:26 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var crypto = require('crypto');
 var Pool = require('pg').Pool;
 var config = {
   user : 'ee16b026',
@@ -98,6 +99,14 @@ app.get('/test-db',function(req,res){
        
    }); 
    
+});
+function hash(input){
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');   
+    return hashed.toString('hex');
+}
+app.get('/hash/:input',function(req,res){
+   var hashString =  hash(req.params.input,'this-is-some-random-string');
+   res.send(hashString);
 });
 var counter = 0;
 app.get("/counter",function(req,res){
